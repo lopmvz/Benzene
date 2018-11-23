9a141a51c727327b31ab1f82cf82e17a5684f415
Marta Lopez Vidal <malop@kemi-d1173424.win.dtu.dk>
2018-11-23 09:41:09 +0100

    up

---
+++ Bz/plot.py
@@ -0,0 +1,130 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+from scipy.interpolate import interp1d
+
+x1shift = - 0.7 # = 285.3 - 286
+
+#datadummy = np.genfromtxt('dummy.txt')
+xdummy = 0 #datadummy[:,0] 
+ydummy = 0 #datadummy[:,1]
+
+data1 = np.genfromtxt('benzene_xas_6311_2ppGss_ucC.dat')
+stcks1 = np.genfromtxt('benzene_xas_6311_2ppGss_ucC.txt')
+ip1 = 290.9
+x1 = data1[:,0] 
+y1 = data1[:,1]
+stcksx1 = stcks1[:,0] 
+stcksy1 = stcks1[:,1]
+
+#data2 = np.genfromtxt('b1u.lor')
+#stcks2 = np.genfromtxt('b1u.dat')
+#x2 = data2[:,0] 
+#y2 = data2[:,1]
+#stcksx2 = stcks2[:,0] 
+#stcksy2 = stcks2[:,1]
+#
+#data3 = np.genfromtxt('b2u.lor')
+#stcks3 = np.genfromtxt('b2u.dat')
+#x3 = data3[:,0] 
+#y3 = data3[:,1]
+#stcksx3 = stcks3[:,0] 
+#stcksy3 = stcks3[:,1]
+#
+#data4 = np.genfromtxt('b3u.lor')
+#stcks4 = np.genfromtxt('b3u.dat')
+#x4 = data4[:,0] 
+#y4 = data4[:,1]
+#stcksx4 = stcks4[:,0] 
+#stcksy4 = stcks4[:,1]
+
+#Experiment
+data10 = np.genfromtxt('Bzexp.csv')
+ip10 = 290.20 #aprox
+x10 = data10[:,1] 
+y10 = (data10[:,0])
+f = interp1d(x10, y10, kind='slinear')
+x10new = np.linspace(x10.min(),x10.max(),1000)
+y10new = f(x10new)
+
+params = {'mathtext.default': 'regular' }          
+plt.rcParams.update(params)
+
+f, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=True)
+plt.xlim(278,291.1) #
+yip = 0.55
+
+#plt.suptitle('C$_6$H$_6$')
+plt.xlabel('Excitation energy (eV)')
+plt.ylim(0.0,yip)
+plt.ylabel('                                             Intensity (arb. units)')
+plt.yticks([])
+ax1.plot([ip1+x1shift,ip1+x1shift],[0,yip],'crimson',linewidth=1.0, dashes=[5, 2])
+ax1.plot(x1+x1shift, y1,'crimson', label='S$_0$', linewidth=2)
+#ax1.plot(x2+x1shift, y2*1, 'darkblue', label='S1($n\pi^*$)  x 10', linewidth=2)
+#ax1.plot(x2+x1shift, y2*1, 'darkblue', label='1\B1u', linewidth=2)
+#ax1.plot(x3+x1shift, y3*1, 'g', label='S2 ($\pi\pi^*$)  x 10', linewidth=2)
+#ax1.plot(x3+x1shift, y3*2, 'g', label='2\B2u + 2\B3u', linewidth=2)
+#ax1.plot(x4+x1shift, y4*1, 'g', label='S2 ($\pi\pi^*$)  x 10', linewidth=2)
+#ax1.plot(x4+x1shift, y4*1, 'y', label='2\B3u', linewidth=2)
+ax1.plot(xdummy, ydummy, 'white',      label='$\Delta x$ = %.2f eV' %x1shift, linewidth=2)
+n=len(stcksx1)
+for i in range(n):
+    ax1.plot([stcksx1[i]+x1shift,stcksx1[i]+x1shift],[0,stcksy1[i]*1],'crimson',linewidth=1.0)
+#n=len(stcksx2)
+#for i in range(n):
+#    ax1.plot([stcksx2[i]+x1shift,stcksx2[i]+x1shift],[0,stcksy2[i]*1],'darkblue',linewidth=1.0)
+#n=len(stcksx3)
+#for i in range(n):
+#    ax1.plot([stcksx3[i]+x1shift,stcksx3[i]+x1shift],[0,stcksy3[i]*2],'green',linewidth=1.0)
+#n=len(stcksx4)
+#for i in range(n):
+#    ax1.plot([stcksx4[i]+x1shift,stcksx4[i]+x1shift],[0,stcksy4[i]*1],'y',linewidth=1.0)
+ax1.legend(loc='upper left',fontsize='small')
+
+ax2.plot([ip10,ip10],[0,yip],'k',linewidth=1.0, dashes=[5, 2])
+ax2.plot(x10, y10, 'k', label='Experiment', linewidth=2)
+ax2.legend(loc='upper left',fontsize='small')
+
+f.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+
+plt.savefig('Bz_NEXAFS.pdf')
+plt.savefig('Bz_NEXAFS.png')
+plt.show()
+
+
+#yip = yip + 0.02
+#
+#plt.xlabel('Excitation energy (eV)')
+#plt.ylim(0.0,yip)
+#plt.ylabel('                                                Intensity (arb. units)')
+#plt.yticks([])
+#ax1.plot([ip1+x1shift,ip1+x1shift],[0,yip],'crimson',linewidth=1.0, dashes=[5, 2])
+#plt.plot(x1+x1shift, y1,'crimson', label='S0', linewidth=2)
+#plt.plot(x2+x1shift, y2*10, 'darkblue', label='S1 x 10', linewidth=2)
+#plt.plot(x3+x1shift, y3*10, 'g', label='S2 x 10', linewidth=2)
+#plt.plot(x4, y4/20, 'k', label='Exp.', linewidth=2)
+#
+#ax1.plot(xdummy, ydummy, 'white',      label='$\Delta x$ = %.2f eV' %x1shift, linewidth=2)
+#ax1.legend(loc='upper left',fontsize='small')
+#
+#
+#ax2.plot(x1+x1shift, y1,'crimson', label = 'S0 ($\Delta x$ = %.2f eV)' %x1shift, linewidth=2)
+#ax2.plot([ip1+x1shift,ip1+x1shift],[0,yip],'crimson',linewidth=1.0, dashes=[5, 2])
+#n=len(stcksx1)
+#for i in range(n):
+#    ax2.plot([stcksx1[i]+x1shift,stcksx1[i]+x1shift],[0,stcksy1[i]*1],'crimson',linewidth=1.0)
+##plt.plot([ip,ip],[0,yip],'k',linewidth=1.0, dashes=[5, 2])
+#ax2.plot(x4, y4/20+0.02, 'k', label='S0 Exp.', linewidth=2)
+#ax2.legend(loc='upper left',fontsize='small')
+#
+#f.subplots_adjust(hspace=0)
+#plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+#
+#plt.savefig('Uracil_Sn_C_1.pdf')
+#plt.show()
+#
+#
+#
---
+++ BzCat/plot.py
@@ -0,0 +1,98 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from scipy.interpolate import spline
+from pylab import *
+from scipy.interpolate import interp1d
+
+x1shift = 0#- 1.1
+x11shift = 0#- 1.1
+x2shift = 0#- 1.14
+x22shift = 0#- 1.14
+
+data1 = np.genfromtxt('benzene_xas_6311_2ppGss_ucC.dat')
+stcks1 = np.genfromtxt('benzene_xas_6311_2ppGss_ucC.txt')
+ip1 = 290.9
+x1 = data1[:,0] 
+y1 = data1[:,1]
+stcksx1 = stcks1[:,0] 
+stcksy1 = stcks1[:,1]
+
+data11 = np.genfromtxt('Bzp_unrel_uc2ppss.txt')
+#ip11 = 2911.85
+stcksx11 = data11[:,0] 
+stcksy11 = (data11[:,1])
+data11 = np.genfromtxt('Bzp_unrel_uc2ppss.dat')
+x11 = data11[:,0] 
+y11 = (data11[:,1])
+
+#
+data22 = np.genfromtxt('Bzp_rel_uc2ppss.txt')
+#ip22 = 291.90
+stcksx22 = data22[:,0] 
+stcksy22 = (data22[:,1])
+data22 = np.genfromtxt('Bzp_rel_uc2ppss.dat')
+x22 = data22[:,0] 
+y22 = (data22[:,1])
+
+#Experiment
+#data5 = np.genfromtxt('adenine_C_exp.csv')
+#ip5 = 291.10
+#x5 = data5[:,0] 
+#y5 = (data5[:,1])
+#x5new = np.linspace(x5.min(),x5.max(),500) #300 represents number of points to make between T.min and T.maxy5new = spline(x5,y5,x5new)f = interp1d(x5, y5, kind='slinear')
+#y5new = f(x5new)
+
+params = {'mathtext.default': 'regular' }          
+plt.rcParams.update(params)
+
+#f, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=True)
+#plt.suptitle('C$_6$H$_6^+$')
+
+
+plt.xlim(280,293) 
+yip = 0.5
+plt.xlabel('Excitation energy (eV)')
+plt.ylabel('                                           Intensity (arb. units)')
+plt.ylim(0,yip) 
+#plt.yticks([])
+
+#plt.plot(x1+x1shift, y1, 'crimson', label='Non-planar geometry ( %.2f eV)' %x1shift, linewidth=2)#, linestyle='--')
+#plt.set_title('Neutral geometry')
+plt.plot(x1+x1shift, y1, 'crimson', label='Bz', linewidth=2)#, linestyle='--')
+plt.plot(x11+x11shift, y11, 'darkblue', label='Bz$^+$ unrelaxed', linewidth=2)#, linestyle='--')
+plt.plot(x22+x22shift, y22, 'green', label='Bz$^+$ relaxed', linewidth=2)#, linestyle='--')
+plt.legend(loc='upper right',fontsize='small', framealpha=1)
+plt.plot([ip1+x1shift,ip1+x1shift],[0,yip],'crimson',linewidth=1.0, dashes=[5, 2])
+
+n=len(stcksx1)
+for i in range(n):
+    plt.plot([stcksx1[i]+x1shift,stcksx1[i]+x1shift],[0,stcksy1[i]*1],'crimson',linewidth=1.0)
+n=len(stcksx11)
+for i in range(n):
+    plt.plot([stcksx11[i]+x11shift,stcksx11[i]+x11shift],[0,stcksy11[i]*1],'darkblue',linewidth=1.0)
+n=len(stcksx22)
+for i in range(n):
+    plt.plot([stcksx22[i]+x22shift,stcksx22[i]+x22shift],[0,stcksy22[i]*1],'green',linewidth=1.0)
+
+#ax2.set_title('Cation geometry')
+#n=len(stcksx2)
+#for i in range(n):
+#    ax2.plot([stcksx2[i]+x2shift,stcksx2[i]+x2shift],[0,stcksy2[i]*1],'crimson',linewidth=1.0)
+
+#ax2.plot(x2+x2shift, y2, 'crimson', label='aug-cc-pVDZ', linewidth=2)#, linestyle='--')
+#ax2.plot([ip2+x2shift,ip2+x2shift],[0,yip],'darkblue',linewidth=1.0, dashes=[5, 2])
+#
+#ax2.legend(loc='upper right',fontsize='small', framealpha=1)
+#
+#
+#ax3.plot(x5, y5/4, 'black', label='Experiment' , linewidth=2 )
+#ax3.plot([ip5,ip5],[0,yip],'black',linewidth=1.0, dashes=[5, 2])
+#ax3.legend(loc='upper right',fontsize='small', framealpha=1)
+#
+#f.subplots_adjust(hspace=0)
+#plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+
+plt.savefig('BzCat.pdf')
+plt.show()
